d93b870bb34939a19f08a541231a9ddcff076680
Mikolaj Lukasik <mlukasik@gmail.com>
2020-01-13 19:54:52 +0000

    Custom UIButton and UITextField

---
+++ GitHubFollowers/MLButton.swift
@@ -0,0 +1,46 @@
+//
+//  MLButton.swift
+//  GitHubFollowers
+//
+//  Created by Mikolaj Lukasik on 13/01/2020.
+//  Copyright © 2020 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class MLButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - CUSTOM INITIALISER
+    
+    init(withColor backgroundColor: UIColor, withTitle title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    // MARK: - CUSTOM FUNCS
+
+    
+    private func configure() {
+        layer.cornerRadius          = 10
+        titleLabel?.textColor       = .white
+        titleLabel?.font            = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+
+}
