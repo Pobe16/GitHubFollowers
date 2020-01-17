77b3dd38d304ff591c3e20808d01935db7c70f50
Mikolaj Lukasik <mlukasik@gmail.com>
2020-01-17 22:03:19 +0000

    Flow layout
    +variable columns number by screen width

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -12,10 +12,13 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var collectionView: UICollectionView!
+    var numberOfColumns: Int = 3
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        determineNumberOfColumns()
         configureViewController()
         getFollowers()
         configureCollectionView()
@@ -28,9 +31,28 @@ class FollowerListVC: UIViewController {
     }
     
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        determineNumberOfColumns()
+        configureCollectionView()
+    }
+    
+    
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func determineNumberOfColumns() {
+        let horizontalSize = self.traitCollection.horizontalSizeClass
+        switch horizontalSize {
+        case .compact:
+            numberOfColumns = 3
+        case .regular:
+            numberOfColumns = 5
+        default:
+            numberOfColumns = 3
+        }
     }
     
     
@@ -50,11 +72,33 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createColumnFlowLayout(numberOfColumns: numberOfColumns))
         view.addSubview(collectionView)
+        
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
+    }
+    
+    func createColumnFlowLayout(numberOfColumns: Int) -> UICollectionViewFlowLayout {
+        let width                           = view.bounds.width
+        let padding: CGFloat                = 12
+        let minimumItemSpacing: CGFloat     = 10
+        
+        let totalPadding                    = 2 * padding
+        let numberOfSpaces                  = numberOfColumns - 1
+        let totalSpacing                    = minimumItemSpacing * CGFloat(numberOfSpaces)
+        let availableWidth                  = width - totalPadding - totalSpacing
+        
+        let itemWidth                       = availableWidth / CGFloat(numberOfColumns)
+        
+        let flowLayout                      = UICollectionViewFlowLayout()
+        flowLayout.sectionInset             = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize                 = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
 
 }
