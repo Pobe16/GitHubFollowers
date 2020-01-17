072d806f93651d9f2ac005916e675e4f7e6e33a3
Mikolaj Lukasik <mlukasik@gmail.com>
2020-01-17 12:09:25 +0000

    Initiali setup for UICollectionView

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -11,18 +11,33 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
+        configureViewController()
+        getFollowers()
+        configureCollectionView()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
-        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
-            
             case .success(let followers):
                 print("Number of followers: \(followers.count)")
                 print(followers)
@@ -34,11 +49,11 @@ class FollowerListVC: UIViewController {
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
 
