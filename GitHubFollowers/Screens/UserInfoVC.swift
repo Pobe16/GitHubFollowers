8eff787d65d329520e976f1bd15d1de19c4ce96e
Mikolaj Lukasik <mlukasik@gmail.com>
2020-01-22 20:59:49 +0000

    User Info network call

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -22,6 +22,18 @@ class UserInfoVC: UIViewController {
         navigationItem.rightBarButtonItem   = doneButton
         
         print(username ?? "not set yet")
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentMLAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "🙁")
+                break
+            }
+            
+        }
 
     }
     
