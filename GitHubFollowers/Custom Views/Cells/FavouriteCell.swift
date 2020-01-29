f690100fee6d6957f01732ba8079532c3f411352
Mikolaj Lukasik <mlukasik@gmail.com>
2020-01-29 23:15:48 +0000

    Favourite Cell

---
+++ GitHubFollowers/Custom Views/Cells/FavouriteCell.swift
@@ -0,0 +1,55 @@
+//
+//  FavouriteCell.swift
+//  GitHubFollowers
+//
+//  Created by Mikolaj Lukasik on 29/01/2020.
+//  Copyright © 2020 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteCell: UITableViewCell {
+    
+    static let reuseID = "FavouriteCell"
+    
+    let avatarImageView     = MLAvatarImageView(frame: .zero)
+    let usernameLabel       = MLTitleLabel(textAlignment: .left, fontSize: 26)
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func set(favourite: Follower) {
+        usernameLabel.text = favourite.login
+        avatarImageView.downloadImage(from: favourite.avatarUrl)
+    }
+    
+    
+    private func configure() {
+        
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType               = .disclosureIndicator
+        
+        let padding: CGFloat        = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding*2),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+    }
+}
